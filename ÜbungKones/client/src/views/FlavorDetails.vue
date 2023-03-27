03412b5b76f1404c420d629b3a0b655a56935532
hochstegerL03 <72703264+hochstegerL03@users.noreply.github.com>
2023-03-27 18:44:21 +0200

    v.0.1

--- ÜbungKones/client/src/views/FlavorDetails.vue
+++ ÜbungKones/client/src/views/FlavorDetails.vue
@@ -13,10 +13,10 @@ onMounted(async () => {
 });
 </script>
 <template>
-  <div class="row justify-center q-mt-md q-ma-lg">
+  <div v-if="flavors" class="row justify-center q-mt-md q-ma-lg">
     <div class="col-12 q-mb-md text-center text-h5 text-weight-bold">{{ flavors.name }}</div>
     <div class="col-6"><q-img :src="`http://localhost:3000/images/${flavors.image}`"></q-img></div>
     <div class="col-12 q-my-md text-center text-h4">Price: {{ flavors.price }}€</div>
-    <q-btn class="bg-secondary  text-white" label="Back" to="/flavors" />
+    <q-btn class="bg-secondary text-white" label="Back" to="/flavors" />
   </div>
 </template>
